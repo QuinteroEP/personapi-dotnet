5c45fac4386ea493adf100f7e79ebb682b5d6a73
Juan Diego Romero <juand.romero@javeriana.edu.co>
2025-10-31 19:09:50 -0500

    arreglar la coneccion del front y el back

--- personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
+++ personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f136e30dc5338af94b84b1dd4d9a4a93874a7ed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9436e06615a5ec8e06574815769bd736f42faf0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
