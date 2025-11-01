e490c17fb55c885b586ae131d3d513377acc5333
Pablo Enrique Quintero Callamand <pabloenriquequintero@gmail.com>
2025-10-31 21:45:21 -0500

    v0.5
    Cambio de Script, tabla y conteo de personas

--- personapi-dotnet/Controllers/PersonaController.cs
+++ personapi-dotnet/Controllers/PersonaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using personapi_dotnet.Models.Entities;
+using System.Linq;
 
 namespace personapi_dotnet.Controllers
 {
@@ -14,7 +15,60 @@ namespace personapi_dotnet.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Personas.ToListAsync());
+            var personas = await _context.Personas
+                .Include(p => p.Telefonos)
+                .ToListAsync();
+
+            return View(personas);
+        }
+
+        public async Task<IActionResult> getTelefono()
+        {
+            var persona = await _context.Personas
+                .Include(p => p.Telefonos)
+                .FirstOrDefaultAsync();
+
+            var telefonos = await _context.Telefonos
+                .Where(t => t.Duenio.Equals(persona))
+                .ToListAsync();
+
+            foreach (var telefono in telefonos)
+            {
+                persona?.Telefonos.Add(telefono);
+            }
+
+            return View(await _context.Telefonos.ToListAsync());
+        }
+
+        public async Task<IActionResult> getUniversidad()
+        {
+            var persona = await _context.Personas
+                .Include(p => p.Estudios)
+                .FirstOrDefaultAsync();
+
+            var universidad = await _context.Estudios
+                .Where(u => u.CcPer.Equals(persona))
+                .ToListAsync();
+
+            foreach (var uni in universidad)
+            {
+                persona?.Estudios.Add(uni);
+            }
+
+            return View(await _context.Estudios.ToListAsync());
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var persona = await _context.Personas
+                .Where(p => p.Cc.Equals(Id))
+                .FirstOrDefaultAsync();
+
+            _context.Estudios.RemoveRange(persona!.Estudios);
+            _context.Personas.Remove(persona!);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
+++ personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9436e06615a5ec8e06574815769bd736f42faf0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c45fac4386ea493adf100f7e79ebb682b5d6a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
